d970748fa6e18f8d2af305354ed5ff55e1005a9d
LeiHaoa <ideahao@outlook.com>
2017-08-28 22:52:47 +0800

    change emacs congif

---
+++ bookmarks.el
@@ -0,0 +1,30 @@
+;;;; Emacs Bookmark Format Version 1 ;;;;
+;;; This format is meant to be slightly human-readable;
+;;; nevertheless, you probably don't want to edit it.
+;;; -*- End Of Bookmark File Format Version Stamp -*-
+(("msta"
+ (filename . "e:/myWork/org/MTSA.org")
+ (front-context-string . "Multivarite Time")
+ (rear-context-string)
+ (position . 1))
+("emacs"
+ (filename . "e:/myWork/org/emacs.org")
+ (front-context-string . "emacs \n\n* 快捷键\n**")
+ (rear-context-string)
+ (position . 1))
+("rl"
+ (filename . "e:/myWork/org/rl.org")
+ (front-context-string . "\n* 增强学习RL的学习笔记\n*")
+ (rear-context-string)
+ (position . 1))
+("LSTM"
+ (filename . "e:/myWork/org/LSTM.org")
+ (front-context-string)
+ (rear-context-string)
+ (position . 9))
+("org-capture-last-stored"
+ (filename . "~/dailyRecord/agenda.org")
+ (front-context-string . "**** TODO 跟宝宝要移动")
+ (rear-context-string . "<2017-02-19 周日>\n")
+ (position . 491))
+)
